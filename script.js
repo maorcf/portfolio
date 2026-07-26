ca13c52df93ebb17c1c0a10b6b622d7072a3b4b2
Maor Cohen Falah <maor.cohen.f@gmail.com>
2026-07-26 15:51:59 +0300

    Revert "Make header sticky across full page scroll on all pages"
    This reverts commit d8d4c9c37a6ce53b567e6ea2091d90aae51b37c3.

--- script.js
+++ script.js
@@ -6,12 +6,3 @@ function closeMobileNav(){
   document.getElementById('mobileNav').classList.remove('open');
   document.getElementById('mobileNavOverlay').classList.remove('open');
 }
-
-function updateHeaderHeightVar(){
-  var header = document.querySelector('header');
-  if(header){
-    document.documentElement.style.setProperty('--header-h', header.offsetHeight + 'px');
-  }
-}
-updateHeaderHeightVar();
-window.addEventListener('resize', updateHeaderHeightVar);
