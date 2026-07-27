6605f1bbe442656db22ef2686ac91de0793d0308
Maor Cohen Falah <maor.cohen.f@gmail.com>
2026-07-27 14:41:44 +0300

    Add mouse-follow parallax effect to hero headline

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ if(floatingNav || backToTop){
     if(backToTop) backToTop.classList.toggle('visible', pastThreshold);
   });
 }
+
+var heroSection = document.querySelector('.hero');
+var heroHeading = heroSection ? heroSection.querySelector('h1') : null;
+if(heroSection && heroHeading){
+  heroSection.addEventListener('mousemove', function(e){
+    var rect = heroSection.getBoundingClientRect();
+    var relX = (e.clientX - rect.left) / rect.width - 0.5;
+    var relY = (e.clientY - rect.top) / rect.height - 0.5;
+    var moveX = relX * 40;
+    var moveY = relY * 20;
+    heroHeading.style.transform = 'translate(' + moveX.toFixed(1) + 'px, ' + moveY.toFixed(1) + 'px)';
+  });
+  heroSection.addEventListener('mouseleave', function(){
+    heroHeading.style.transform = 'translate(0px, 0px)';
+  });
+}
