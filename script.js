aa6cd0e794ac6d1d1a1b7af0e714476888afbca9
Maor Cohen Falah <maor.cohen.f@gmail.com>
2026-07-30 13:21:23 +0300

    Hide floating header when scrolling up, show it when scrolling down

--- script.js
+++ script.js
@@ -11,13 +11,17 @@ var floatingNav = document.querySelector('.floating-nav');
 var backToTop = document.querySelector('.back-to-top');
 if(floatingNav || backToTop){
   var scrollTicking = false;
+  var lastScrollY = window.scrollY;
   window.addEventListener('scroll', function(){
     if(scrollTicking) return;
     scrollTicking = true;
     requestAnimationFrame(function(){
-      var pastThreshold = window.scrollY > 200;
-      if(floatingNav) floatingNav.classList.toggle('visible', pastThreshold);
+      var currentY = window.scrollY;
+      var pastThreshold = currentY > 200;
+      var scrollingDown = currentY > lastScrollY;
+      if(floatingNav) floatingNav.classList.toggle('visible', pastThreshold && scrollingDown);
       if(backToTop) backToTop.classList.toggle('visible', pastThreshold);
+      lastScrollY = currentY;
       scrollTicking = false;
     });
   }, { passive: true });
