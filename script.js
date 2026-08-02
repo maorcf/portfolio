987820368b3d560fdb028fb2df203d77e201bc00
Maor Cohen Falah <maor.cohen.f@gmail.com>
2026-08-02 15:21:11 +0300

    Add sideways hover shift animation to hero heading letters

--- script.js
+++ script.js
@@ -27,3 +27,31 @@ if(floatingNav || backToTop){
   }, { passive: true });
 }
 
+(function(){
+  var heroSection = document.querySelector('.hero');
+  var heading = heroSection ? heroSection.querySelector('h1') : null;
+  if(!heroSection || !heading) return;
+
+  function wrapChars(el){
+    Array.prototype.forEach.call(Array.prototype.slice.call(el.childNodes), function(node){
+      if(node.nodeType === 3){
+        var frag = document.createDocumentFragment();
+        node.textContent.split('').forEach(function(ch){
+          if(ch === ' '){
+            frag.appendChild(document.createTextNode(' '));
+          } else {
+            var span = document.createElement('span');
+            span.className = 'char';
+            span.textContent = ch;
+            frag.appendChild(span);
+          }
+        });
+        node.parentNode.replaceChild(frag, node);
+      } else if(node.nodeType === 1 && node.tagName !== 'BR' && !node.classList.contains('icon-chip')){
+        wrapChars(node);
+      }
+    });
+  }
+  wrapChars(heading);
+})();
+
